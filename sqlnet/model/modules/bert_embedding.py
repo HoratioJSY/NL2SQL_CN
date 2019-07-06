5804827aa1ce7e1606e9ea41f6284bc47f78548d
Horatio J.S.Y <horatio.j.s.y@Gmail.com>
2019-07-06 16:47:24 +0800

    add cuda support to bert inference

--- sqlnet/model/modules/bert_embedding.py
+++ sqlnet/model/modules/bert_embedding.py
@@ -16,6 +16,8 @@ class BertEmbedding(nn.Module):
         self.bert_model = BertModel.from_pretrained(bert_path)
         self.Token2ID = self.load_vocab(bert_path + 'vocab.txt')
         self.bert_model.eval()
+        if self.gpu:
+            self.bert_model.to('cuda')
 
     def load_vocab(self, vocab_file):
         """Loads a vocabulary file into a dictionary."""
@@ -34,13 +36,10 @@ class BertEmbedding(nn.Module):
     def gen_x_batch(self, q):
         B = len(q)
         tokenizered_q = []
-        tokens_id_list = []
         val_len = np.zeros(B, dtype=np.int64)
 
         # 一条Query及对应的表头，表头为列名
         for i, one_q in enumerate(q):
-            # print('one_q', one_q)
-            # one_token = ['[CLS]'] + self.tokenizer.tokenize(''.join(one_q)) + ['[SEP]']
             contain_token = []
             for tok in one_q:
                 if self.Token2ID.__contains__(tok):
@@ -49,28 +48,27 @@ class BertEmbedding(nn.Module):
                     contain_token.append('[UNK]')
 
             one_token = ['[CLS]'] + contain_token + ['[SEP]']
-            # print('one_token', one_token)
             assert len(one_q)+2 == len(one_token)
+
             tokenizered_q.append(one_token)
-            tokens_id_list.append(self.tokenizer.convert_tokens_to_ids(one_token))
             val_len[i] = len(one_token)
         max_len = max(val_len)
-        # print(val_len)
-        # print('max len:', max_len)
 
-        tokens_id_array = np.zeros([B, max_len], dtype=np.int64)
-        for i in range(B):
-            for t in range(len(tokens_id_list[i])):
-                tokens_id_array[i, t] = tokens_id_list[i][t]
-        tokens_id_tensor = torch.from_numpy(tokens_id_array)
-        segments_tensor = torch.zeros([B, max_len], dtype=torch.int64)
+        tokens_id_tensor = torch.zeros([B, max_len], dtype=torch.int64)
+        for i, sentence in enumerate(tokenizered_q):
+            tokens_id_tensor[i, :val_len[i]] = torch.tensor([self.tokenizer.convert_tokens_to_ids(sentence)])
+
+        # tokens if used in attention, The mask has 1 for real tokens and 0 for padding tokens
+        attention_mask = torch.zeros([B, max_len], dtype=torch.long)
+        for i, sample_length in enumerate(val_len):
+            attention_mask[i, :sample_length] = torch.ones([1, sample_length], dtype=torch.long)
 
         if self.gpu:
-            tokens_id_tensor = tokens_id_tensor.cuda()
-            segments_tensor = segments_tensor.cuda()
+            tokens_id_tensor = tokens_id_tensor.to('cuda')
+            attention_mask = attention_mask.to('cuda')
 
         with torch.no_grad():
-            hidden_state, _ = self.bert_model(tokens_id_tensor, segments_tensor, output_all_encoded_layers=False)
+            hidden_state, _ = self.bert_model(tokens_id_tensor, attention_mask=attention_mask, output_all_encoded_layers=False)
         # print('hidden state size1:', hidden_state.shape)
         return hidden_state, val_len
 
@@ -91,7 +89,6 @@ class BertEmbedding(nn.Module):
 
         val_len = np.zeros(B, dtype=np.int64)
         tokenizered_chr = []
-        tokens_id = []
 
         for i, one_str in enumerate(str_list):
 
@@ -101,25 +98,24 @@ class BertEmbedding(nn.Module):
                     contain_token.append(tok)
                 else:
                     contain_token.append('[UNK]')
-
             tokenizered_chr.append(['[CLS]'] + contain_token + ['[SEP]'])
-            tokens_id.append(self.tokenizer.convert_tokens_to_ids(tokenizered_chr[-1]))
             val_len[i] = len(tokenizered_chr[-1])
         max_len = max(val_len)
 
-        tokens_id_array = np.zeros((B, max_len), dtype=np.int64)
-        for i in range(B):
-            for t in range(len(tokens_id[i])):
-                tokens_id_array[i, t] = tokens_id[i][t]
-        tokens_id_tensor = torch.from_numpy(tokens_id_array)
-        segments_tensor = torch.zeros([B, max_len], dtype=torch.int64)
+        tokens_id_tensor = torch.zeros([B, max_len], dtype=torch.int64)
+        for i, sentence in enumerate(tokenizered_chr):
+            tokens_id_tensor[i, :val_len[i]] = torch.tensor([self.tokenizer.convert_tokens_to_ids(sentence)])
+
+        attention_mask = torch.zeros([B, max_len], dtype=torch.long)
+        for i, sample_length in enumerate(val_len):
+            attention_mask[i, :sample_length] = torch.ones([1, sample_length], dtype=torch.long)
 
         if self.gpu:
-            tokens_id_tensor = tokens_id_tensor.cuda()
-            segments_tensor = segments_tensor.cuda()
+            tokens_id_tensor = tokens_id_tensor.to('cuda')
+            attention_mask = attention_mask.to('cuda')
 
         with torch.no_grad():
-            hidden_state, _ = self.bert_model(tokens_id_tensor, segments_tensor, output_all_encoded_layers=False)
+            hidden_state, _ = self.bert_model(tokens_id_tensor, attention_mask=attention_mask, output_all_encoded_layers=False)
         # print('hidden state size2:', hidden_state.shape)
 
         return hidden_state, val_len
